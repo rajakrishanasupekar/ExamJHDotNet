4ee99484e2040d310ecbb5b97e10f532db2903bf
Rajakrishana Supekar <93500264+rajakrishanasupekar@users.noreply.github.com>
2022-02-19 18:15:02 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Rate { get; set; }
+        public string Description  { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
